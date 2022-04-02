54210a9a87cf861171e8d6a0a2ff48d6edec43cd
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-04-02 11:56:24 +1100

    a

--- yr4/javascript.js
+++ yr4/javascript.js
@@ -2,12 +2,13 @@
 
 // this script uses the LocalStorage element, rather than a cookie
 // This means that it is accessible across all pages, and not just the current page.
+
 var username = localStorage.getItem("username");
       document.getElementById("username").innerHTML = username;
       if(!username){
         username = prompt("Please enter your name!");
+        location.reload();
         localStorage.setItem("username", username);
         
       }
-
 // END: Javascript code for sourcing, and storing the name of the user who enters the site. 
