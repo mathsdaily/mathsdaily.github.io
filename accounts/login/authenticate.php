f4477db3950ec14ffda31be1f35aa315adf9654a
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-04-02 16:28:37 +1100

    Update authenticate.php

--- accounts/login/authenticate.php
+++ accounts/login/authenticate.php
@@ -57,3 +57,34 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 }
 ?>
 
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PHP is not running, redirection instead please</title>
+		
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
+
+	</head>
+
+ 
+
+
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Maths Daily</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Home Page</h2>
+			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+		</div>
+	</body>
+</html>
+
