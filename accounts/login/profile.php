0972e70ed532b9398063114478506199a559ec90
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-04-02 16:29:50 +1100

    Update profile.php

--- accounts/login/profile.php
+++ accounts/login/profile.php
@@ -29,14 +29,14 @@ $stmt->close();
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Wind Cloud - Profile Page</title>
+		<title>Maths Daoly - Profile Page</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Wind Cloud</h1>
+				<h1>Maths Daily</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
