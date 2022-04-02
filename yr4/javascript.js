26222a493b779ca4e79ce7dea1aea3f0101fa636
James Coates <69493073+nexuspcs@users.noreply.github.com>
2022-04-02 15:54:53 +1100

    a

--- yr4/javascript.js
+++ yr4/javascript.js
@@ -6,9 +6,14 @@
 var username = localStorage.getItem("username");
       document.getElementById("username").innerHTML = username;
       if(!username){
-        username = prompt("Please enter your name!");
+        username = prompt("It looks like we can't find your name. Please enter your name");
         location.reload();
         localStorage.setItem("username", username);
         
+      }
+      if(username == null) {
+        username = prompt("It looks like we can't find your name. Please enter your name");
+        location.reload();
+        localStorage.setItem("username", username);
       }
 // END: Javascript code for sourcing, and storing the name of the user who enters the site. 
