04ff84f94b4b9eba07e5bd3f3cb9a4fd6e193328
npcs <69493073+nexuspcs@users.noreply.github.com>
2022-08-01 13:10:03 +1000

    Fixed root

---
+++ yr5/modal1/javascript.js
@@ -0,0 +1,90 @@
+
+const problemElement = document.querySelector(".problem")
+const ourForm = document.querySelector(".our-form")
+const ourField = document.querySelector(".our-field")
+const pointsNeeded = document.querySelector(".points-needed")
+const mistakesAllowed = document.querySelector(".mistakes-allowed")
+const progressBar = document.querySelector(".progress-inner")
+const endMessage = document.querySelector(".end-message")
+const resetButton = document.querySelector(".reset-button")
+
+let state = {
+  score: 0,
+  wrongAnswers: 0
+}
+
+function updateProblem() {
+  state.currentProblem = generateProblem()
+  problemElement.innerHTML = `${state.currentProblem.numberOne} ${state.currentProblem.operator} ${state.currentProblem.numberTwo}`
+  ourField.value = ""
+  ourField.focus()
+}
+
+updateProblem()
+
+function generateNumber(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+function generateProblem() {
+  return {
+    numberOne: generateNumber(11),
+    numberTwo: generateNumber(11),
+    operator: ['÷'][generateNumber(0)]
+  }
+}
+ourForm.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  let correctAnswer
+  const p = state.currentProblem
+  if (p.operator == "÷") correctAnswer = Math.round (p.numberOne / p.numberTwo)
+  if (correctAnswer === Infinity) correctAnswer = 0
+
+  if (parseInt(ourField.value, 10) === correctAnswer) {
+    state.score++
+    pointsNeeded.textContent = 10 - state.score
+    updateProblem()
+    renderProgressBar()
+  } else {
+    state.wrongAnswers++
+    mistakesAllowed.textContent = 2 - state.wrongAnswers
+    problemElement.classList.add("animate-wrong")
+    setTimeout(() => problemElement.classList.remove("animate-wrong"), 451)
+  }
+  checkLogic()
+}
+
+function checkLogic() {
+  // if you won
+  if (state.score === 10) {
+    endMessage.textContent = "Congrats! You won."
+    document.body.classList.add("overlay-is-open")
+    setTimeout(() => resetButton.focus(), 331)
+  }
+
+  // if you lost
+  if (state.wrongAnswers === 3) {
+    endMessage.textContent = "Good effort, but let's try again"
+    document.body.classList.add("overlay-is-open")
+    setTimeout(() => resetButton.focus(), 331)
+  }
+}
+
+resetButton.addEventListener("click", resetGame)
+
+function resetGame() {
+  document.body.classList.remove("overlay-is-open")
+  updateProblem()
+  state.score = 0
+  state.wrongAnswers = 0
+  pointsNeeded.textContent = 10
+  mistakesAllowed.textContent = 2
+  renderProgressBar()
+}
+
+function renderProgressBar() {
+  progressBar.style.transform = `scaleX(${state.score / 10})`
+}
